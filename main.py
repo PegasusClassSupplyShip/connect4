c8008ab8731b28cb05b33c4afc4a4ab9f8f9719d
boobooBaby <boobooBaby.com>
2018-02-10 14:42:44 +0000

    board setup function and board printing function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def main():
+def setUpBoard():
     #set up board
     board=[]
     for i in range(6):
@@ -7,8 +7,15 @@ def main():
         for j in range(7):
             #width is 7
             board[i].append("")
-    print(board)
+    return board
 
+def printBoard(board):
+    for i in range(len(board)):
+        print(board[i])
+        
+def main():
+    board=setUpBoard()
+    printBoard(board)
 
 if(__name__=="__main__"):
     main()
