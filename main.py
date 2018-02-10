a10036d79bf93b3f3ecdba62d64bbbb5623fb3fe
boobooBaby <boobooBaby.com>
2018-02-10 15:11:57 +0000

    Now Playable with 2 people
    printBoard function modified to print index
    makeMove function created to make moves
    input requested by main function then result is displayed
    move validation does not exist
    does not check if game have ended

--- main.py
+++ main.py
@@ -6,16 +6,50 @@ def setUpBoard():
         board.append([])
         for j in range(7):
             #width is 7
-            board[i].append("")
+            board[i].append(" ")
     return board
 
 def printBoard(board):
+    #display board
+    horizontalIndex=[]
+    for i in range(len(board[0])):
+        horizontalIndex.append(str(i))
+    print(" ",horizontalIndex)
     for i in range(len(board)):
-        print(board[i])
-        
+        print(str(i), board[i])
+
+def makeMove(playerTurn,inputNum,board):
+    deepestIndex=len(board)-1
+    chipPlaced=False
+    while(chipPlaced==False):
+        if(board[deepestIndex][inputNum]==" "):
+            board[deepestIndex][inputNum]=playerTurn
+            chipPlaced=True
+        deepestIndex-=1
+    return board
+
 def main():
     board=setUpBoard()
+    print("TURN:0")
     printBoard(board)
-
+    gameEnded=False
+    #plater turn is "R" or "Y" for red or yellow
+    playerTurn="R"
+    numberOfTurnsPlayed=0
+    while(gameEnded==False):
+        #getInPut
+        inputNum=eval(input(playerTurn + " player, please enter input: "))
+        #makeMove
+        board=makeMove(playerTurn,inputNum,board)
+        #printBoard
+        print("")
+        numberOfTurnsPlayed+=1
+        print("TURN:",numberOfTurnsPlayed)
+        printBoard(board)
+        if (playerTurn=="R"):
+            playerTurn="Y"
+        else:
+            playerTurn="R"
+            
 if(__name__=="__main__"):
     main()
